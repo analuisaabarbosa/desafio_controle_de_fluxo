7db03422c839bc1be794d6b99b57aaa9662351e3
analuisa <analuisaaugustob@gmail.com>
2024-06-17 15:52:00 -0300

    :sparkles:feat:Contador e Exceptions.

---
+++ controle_fluxo/src/contador/Contador.java
@@ -0,0 +1,36 @@
+package contador;
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+
+        Scanner terminal = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número: ");
+
+        int primeiroParametro = terminal.nextInt();
+
+        System.out.println("Digite o segundo número: ");
+        int segundoParametro = terminal.nextInt();
+
+        try {
+            if (primeiroParametro >= segundoParametro) {
+                throw new ParametrosInvalidosException("O segundo número deve ser menor que o primeiro.");
+            }
+            contar(primeiroParametro, segundoParametro);
+        } catch (ParametrosInvalidosException exception) {
+            System.out.println(exception.getMessage());
+        }finally {
+            terminal.close();
+        }
+
+    }
+
+    static void contar(int primeiroParametro, int segundoParametro) throws ParametrosInvalidosException{
+
+        int contagem = segundoParametro - primeiroParametro;
+        for(int i = 1; i <= contagem; i++){
+            System.out.println("Imprimindo números: " + i);
+        };
+    }
+}
